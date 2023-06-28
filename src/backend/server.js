0f253d0f92a73676f31825971288ab3525563ac3
Asaoka Takuya <dimas080201@gmail.com>
2023-06-28 12:07:58 +0900

    update:server.js最新化

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,41 +1,58 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const environment = process.env.NODE_ENV || "development";
-const config = require("./knexfile.js")[environment];
-const knex = require("knex")(config);
-require("dotenv").config();
-// require("dotenv")は、.envファイルの内容を読み込むためのNode.jsモジュールであり、config()メソッドを呼び出すことで.envファイルの内容を環境変数に設定
-
-const port = process.env.PORT || 3001;
-
+const environment = process.env.NODE_ENV || 'development';
+const config = require('./knexfile.js')[environment];
+const knex = require('knex')(config);
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-
 app.use(express.json());
-
+// GET ユーザーテーブルを返す
+app.get('/api/users', async (req, res) => {
+  await knex('USER')
+    .select()
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
 // GET 県別いいね数取得
-app.get("/api/favorites", async (req, res) => {
-  await knex("FAVORITE")
+app.get('/api/favorites', async (req, res) => {
+  await knex('FAVORITE')
     .select()
     .then((favorites) => {
       const result = [];
-      const countMap = new Map();
-      // 県ごとにヒット数をカウント
-      for (const favorite of favorites) {
-        if (countMap.has(favorite.prefecture)) {
-          countMap.set(prefecture, countMap.get(prefecture) + 1);
+      favorites.forEach((favorite) => {
+        let flag = false;
+        // すでに県がresultにあるか確認
+        result.forEach((element) => {
+          if (element.name === favorite.prefecture) {
+            flag = true;
+          }
+        });
+        // 県がすでにresultにあればnumberを加算、なければ県追加
+        if (flag) {
+          result.forEach((element) => {
+            if (element.name === favorite.prefecture) {
+              element.number++;
+            }
+          });
         } else {
-          countMap.set(prefecture, 1);
+          result.push({
+            name: favorite.prefecture,
+            imgSrc: favorite.images[0],
+            number: 1,
+          });
         }
-      }
-      // 県ごとに{prefecture, number}の形でカウント結果をプッシュ
-      for (const [prefecture, number] of countMap.entries()) {
-        result.push({ prefecture, number });
-      }
+      });
       res.status(200).json(result);
     })
     .catch((err) => {
@@ -43,28 +60,35 @@ app.get("/api/favorites", async (req, res) => {
       res.sendStatus(500);
     });
 });
-
 // GET 県別のいいね一覧
-app.get("/api/favorites/:prefecture", async (req, res) => {
+app.get('/api/favorites/:prefecture', async (req, res) => {
   const prefecture = req.params.prefecture;
-  await knex("FAVORITE")
-    .select("id", "title", "images", "price", "access")
+  await knex('FAVORITE')
+    .select('id', 'title', 'images', 'price', 'access')
     .where({ prefecture: prefecture })
     .then((favorites) => {
-      res.status(200).json(favorites);
+      const result = [];
+      favorites.forEach((favorite) => {
+        result.push({
+          id: favorite.id,
+          name: favorite.title,
+          imgSrc: favorite.images[0],
+          price: favorite.price,
+          access: favorite.access,
+        });
+      });
+      res.status(200).json(result);
     })
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
     });
 });
-
 // POST ログイン
-app.post("/api/login", async (req, res) => {
+app.post('/api/login', async (req, res) => {
   const { name, password } = req.body;
-
-  const db = await knex.select("*").from("USER").where;
-  knex("USER")
+  const db = await knex.select('*').from('USER').where;
+  knex('USER')
     .select()
     .where({ name, password })
     .first()
@@ -79,12 +103,10 @@ app.post("/api/login", async (req, res) => {
       res.status(500).json({ error: error.message });
     });
 });
-
 // POST USER追加
-app.post("/api/users", (req, res) => {
+app.post('/api/users', (req, res) => {
   const { name, email, password } = req.body;
-
-  knex("USER")
+  knex('USER')
     .insert({ name, email, password })
     .then(() => {
       res.sendStatus(200);
@@ -94,28 +116,23 @@ app.post("/api/users", (req, res) => {
       res.sendStatus(500);
     });
 });
-
 // POST FAVOTITE追加
-app.post("/api/favorites", (req, res) => {
-
-  knex("FAVORITE").insert(req.body)
-  // knex("FAVORITE").del().where({ title: req.body.title })
+app.post('/api/favorites', (req, res) => {
+  knex('FAVORITE')
+    .insert(req.body)
+    // knex("FAVORITE").del().where({ title: req.body.title })
     .then(() => {
-      res.set("content-type", "application/json")
-         .status(200)
-         .send(req.body);
+      res.set('content-type', 'application/json').status(200).send(req.body);
     })
     .catch((err) => {
       console.error(err);
       res.sendStatus(500);
     });
 });
-
-app.put("/api/users/:id", (req, res) => {
+app.put('/api/users/:id', (req, res) => {
   const { id } = req.params;
   const { name, email } = req.body;
-
-  knex("users")
+  knex('users')
     .where({ id })
     .update({ name, email })
     .then(() => {
@@ -126,11 +143,9 @@ app.put("/api/users/:id", (req, res) => {
       res.sendStatus(500);
     });
 });
-
-app.delete("/api/users/:id", (req, res) => {
+app.delete('/api/users/:id', (req, res) => {
   const { id } = req.params;
-
-  knex("users")
+  knex('users')
     .where({ id })
     .del()
     .then(() => {
@@ -141,7 +156,6 @@ app.delete("/api/users/:id", (req, res) => {
       res.sendStatus(500);
     });
 });
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
